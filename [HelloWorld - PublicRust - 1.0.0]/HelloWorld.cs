8d7efd136cce90a73e58f1ea8e777d73ac1a11fb
muxira <74909168+muxira@users.noreply.github.com>
2025-09-15 03:08:14 +0300

    Upload [HelloWorld - PublicRust - 1.0.0]

---
+++ [HelloWorld - PublicRust - 1.0.0]/HelloWorld.cs
@@ -0,0 +1,22 @@
+using Oxide.Core.Plugins;
+using Oxide.Core.Libraries.Covalence;
+
+namespace Oxide.Plugins
+{
+    [Info("HelloWorld", "PublicRust", "1.0.0")]
+    [Description("Simple Hello World plugin for Rust")]
+
+    public class HelloWorld : RustPlugin
+    {
+        private void Init()
+        {
+            Puts("Hello, Rust World! Plugin successfully loaded.");
+        }
+
+        [ChatCommand("hello")]
+        private void HelloCommand(BasePlayer player, string command, string[] args)
+        {
+            player.ChatMessage("Hello, " + player.displayName + "! Welcome to this Rust server.");
+        }
+    }
+}
